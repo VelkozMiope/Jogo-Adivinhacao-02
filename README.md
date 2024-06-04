523a964f920d5a672ce2ac36db4f846131939926
Márcio <paulin.marcio@outlook.com>
2024-06-04 16:34:18 -0300

    Finalização do projeto

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Jogo-Adivinhacao-02
- Jogo de adivinhação feito com o curso "Lógica de programação: explore funções e listas" pela Alura. 
+# Jogo-Adivinhacao-01
+ Jogo de adivinhação de um número aleatório, criado com o curso [Lógica de programação: explore funções e listas](https://cursos.alura.com.br/course/logica-programacao-funcoes-listas), na plataforma da Alura, como parte do programa Geração Caldeira 2024.
+
+Como uma melhoria do projeto anterior, o objetivo do jogo continua sendo o mesmo, gerar um número aleatório, onde o usuário precisa descobrir o número, com o programa dizendo se o número é maior ou menor, e retornando uma mensagem ao usuário acertar o número.
+
+Este projeto introduziu conceitos como funções, manipulamento de elementos na tela via Javascript, listas e como utilizar o Github para salvar o projeto e o Vercel para torna-lo disponivel online.
---
+++ app.js
@@ -0,0 +1,60 @@
+let listaNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 0;
+
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+};
+
+function exibirMensagemInicial(){
+    exibirTexto('h1', 'Jogo do número secreto');
+    exibirTexto('p', 'Escolha um número entre 1 e 10');
+};
+
+function gerarNumeroAleatorio(){
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeElementosLista = listaNumerosSorteados.length;
+
+    if (quantidadeElementosLista == numeroLimite) listaNumerosSorteados = [];
+
+    if (listaNumerosSorteados.includes(numeroEscolhido)) return gerarNumeroAleatorio() 
+    else {
+        listaNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+};
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+};
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    tentativas++;
+    if (chute == numeroSecreto) {
+        let textoTentativa = tentativas > 1 ? 'tentativa' : 'tentativas';
+        let mensagemTentativa = `Você descobriu o número ${numeroSecreto} em ${tentativas} ${textoTentativa}!`
+        exibirTexto('h1', 'Parabéns!');
+        exibirTexto('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute < numeroSecreto) exibirTexto('p', 'O número secreto é maior!');
+        
+        else exibirTexto('p', 'O número secreto é menor!');
+    }
+    limparCampo();
+};
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 0;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+};
+
+exibirMensagemInicial();
